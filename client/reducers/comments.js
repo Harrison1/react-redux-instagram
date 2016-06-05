81efa639f904698715a660cdf8c9d5f4ecb76b98
Harrison <hmcguire13@gmail.com>
2016-06-05 16:43:06 -0700

    working app

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,5 +1,31 @@
+function postComments(state=[], action) {
+	switch(action.type) {
+		case 'ADD_COMMENT':
+			//return existing state and the new comment
+			return [...state, {
+				user: action.author,
+				text: action.comment
+			}];
+		case 'REMOVE_COMMENT':
+			// return the state without the comment
+			return [
+				...state.slice(0,action.i),
+				...state.slice(action.i + 1)
+			]
+		default:
+			return state;
+	}
+}
+
 function comments(state=[], action) {
-	console.log(state, action);
+	if(typeof action.postId !== 'undefined') {
+		return {
+			// take the current state
+			...state,
+			// overrite this post with a new one
+			[action.postId]: postComments(state[action.postId], action)
+		}
+	}
 	return state;
 }
 
--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,8 +1,17 @@
 // reducer takes the action (info of what happened) and a copy of the current state
 function posts(state=[], action) {
-	console.log('the post will change');
-	console.log(state, action);
-	return state;
+	switch(action.type) {
+		case 'INCREMENT_LIKES':
+			console.log("Incrementing Likes!!!");
+			const i = action.index;
+			return [
+				...state.slice(0, i), //before the one we are updating
+				{...state[i], likes: state[i].likes + 1},
+				...state.slice(i + 1), //after the one we are updating
+			]
+		default:
+			return state;
+	}
 }
 
 export default posts;
